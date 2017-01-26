75de47cd05c68ed9d8d6488c4f919fab9044a124
Elsa Nyrhinen <elsa.nyrhinen@helsinki.fi>
2017-01-26 21:20:35 +0200

    lisätty tietokantataulut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (name, email, password, rooli) VALUES ('Elsa', 'elsa.nyrhinen@helsinki.fi', 'Elsa123', 'admin');
+INSERT INTO Treeni (name, kesto, soveltuvuus, kuvaus) VALUES ('Crossfit', '30min', 'Juniorit, Seniorit', 'Nopeatempoinen treeni.');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,42 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja (
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+email varchar(50) NOT NULL,
+password varchar(50) NOT NULL,
+rooli varchar
+);
+
+CREATE TABLE Treeni (
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+kesto varchar(50),
+soveltuvuus varchar(50),
+kuvaus varchar(500) 
+);
+
+
+CREATE TABLE Voimalaji (
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+kuvaus varchar(500) 
+);
+
+CREATE TABLE Liike (
+id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL,
+soveltuvuus varchar(50),
+kuvaus varchar(500),
+voimalaji_id INTEGER REFERENCES Voimalaji(id) 
+);
+
+CREATE TABLE Treeniliike (
+liike_id INTEGER REFERENCES Liike(id),
+treeni_id INTEGER REFERENCES Treeni(id)
+);
+
+
+CREATE TABLE Voimatreeni (
+voimalaji_id INTEGER REFERENCES Voimalaji(id),
+treeni_id INTEGER REFERENCES Treeni(id)
+);
